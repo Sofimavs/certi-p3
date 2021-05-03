088e63d10131105712293195b2564f2155c3807a
Sofía Vargas <sofi@Sofias-MacBook-Air-1261.local>
2021-05-03 11:40:47 -0400

    P3-001 Modified files .cs and .json in Practice3

---
+++ Practice3/Controllers/StudentsController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Practica2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        public StudentsController()
+        {
+
+        }
+                [HttpGet]
+        public List<Student> GetStudent()
+        {
+            return new List<Student>(){
+                new Student(){Name = "Sofia Vargas"},
+                new Student(){Name = "Andi Guardia"},
+                new Student(){Name = "Gabriel Perez"},
+                new Student(){Name = "Ale Ledezma"},
+            };
+        }
+       [HttpPost]
+        public Student CreateStudent([FromBody] String studentName, String studentLastName)
+        {
+            return new Student()
+            {
+                Name = studentName,
+            };
+        }
+
+        [HttpPut]
+        public Student UpdateStudent([FromBody] Student student)
+        {
+            student.Name = "updated";
+            return student;
+        }
+
+        [HttpDelete]
+        public Student DeleteStudent([FromBody] Student student)
+        {
+            student.Name = "deleted";
+            return student;
+        }
+    }
+}
---
+++ Practice3/Student.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Practica2
+{
+    public class Student
+    {
+        public String Name { get; set;}
+    }
+}
